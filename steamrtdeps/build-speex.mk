93bcdebc268744e12ba70543638f2aa368ba2148
Shimizu Hikaru <MuPendulum@users.noreply.github.com>
2026-07-17 15:29:57 +0900

    speex: add speexdsp as a build dependency

--- steamrtdeps/build-speex.mk
+++ steamrtdeps/build-speex.mk
@@ -2,11 +2,15 @@
 ## speex
 ##
 
+include $(SRC)/steamrtdeps/build-speexdsp.mk
+
 SPEEX_CONFIGURE_ARGS := \
 	--enable-shared \
 	--disable-static \
 	--disable-binaries \
 	--disable-valgrind
+
+SPEEX_DEPENDS = $(SPEEXDSP_DEPENDENCY)
 
 $(eval $(call rules-source,speex,$(SRCDIR)/steamrtdeps/speex))
 $(eval $(call rules-configure,speex,i386,unix))
